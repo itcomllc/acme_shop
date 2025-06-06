cbcb67dcaf490baea78c504706a0e2f16d6de198
Atsuji Okuyama <okuyama@workgroup.jp>
2025-06-06 15:48:48 +0900

    何やってるんだ・・

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,7 @@
+@props(['title'])
+
 <x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
+    <flux:main class="bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
         {{ $slot }}
     </flux:main>
 </x-layouts.app.sidebar>
