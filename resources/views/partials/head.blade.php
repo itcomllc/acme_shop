cdd4279fe094cc4672fee4c14518bcf41a6646c3
Atsuji Okuyama <okuyama@workgroup.jp>
2025-06-06 15:35:20 +0900

    csrfトークン

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="theme" content="{{ session('theme', 'system') }}">
 
 <title>{{ $title ?? config('app.name') }}</title>
 
